2499dd8ad3919ac6ed830d188d9a64add4b10d50
Francisco Penedo <fran.penedo@gmail.com>
2016-09-22 17:41:31 -0400

    Finished 2D figure, fixed bug in first_change

--- femformal/tests/util_tests.py
+++ femformal/tests/util_tests.py
@@ -1,5 +1,7 @@
 import femformal.util as u
 import numpy as np
+import networkx as nx
+import itertools as it
 
 import logging
 logger = logging.getLogger('FEMFORMAL')
@@ -10,4 +12,21 @@ def labels_test():
     l = u.state_label(x)
     assert l == l_exp
     np.testing.assert_array_equal(u.label_state(l), x)
+
+
+def draw_ts_2D_test():
+    partition = [list(range(5)) for i in [0,1]]
+    ts = nx.DiGraph()
+    indices = [list(range(len(p) - 1)) for p in partition]
+    states = list(it.product(*indices))
+    ts.add_nodes_from([u.state_label(s) for s in states])
+    ts.add_edge(u.state_label([2,2]), u.state_label([3,2]))
+    ts.add_edge(u.state_label([2,2]), u.state_label([2,3]))
+    ts.add_edge(u.state_label([2,2]), u.state_label([1,2]))
+    ts.add_edge(u.state_label([2,2]), u.state_label([2,1]))
+    ts.add_edge(u.state_label([3,2]), u.state_label([2,2]))
+    ts.add_edge(u.state_label([2,2]), u.state_label([2,2]))
+
+    # u.draw_ts_2D(ts, partition)
+
 
--- femformal/util.py
+++ femformal/util.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 import logging
 logger = logging.getLogger('FEMFORMAL')
@@ -24,7 +25,10 @@ def long_first(a, b):
         return b, a
 
 def first_change(a, b):
-    return next(((i, x[1] - x[0]) for i, x in enumerate(zip(a, b)) if x[0] != x[1]))
+    try:
+        return next(((i, x[1] - x[0]) for i, x in enumerate(zip(a, b)) if x[0] != x[1]))
+    except StopIteration:
+        return 0, 0
 
 
 def subst_spec_labels(spec, regions):
@@ -67,26 +71,32 @@ def draw_ts_2D(ts, partition):
 def _draw_edge(e, partition, ax):
     f = label_state(e[0])
     t = label_state(e[1])
-    i, d = first_change(t, f)
+    i, d = first_change(f, t)
 
     fcenter, fsize = _box_dims(partition, f)
     tcenter, tsize = _box_dims(partition, t)
 
     if d != 0:
-        fcenter[i] += fsize[0] / 4.0
-        tcenter[i] += fsize[0] / 4.0
-        fcenter[i] += d * fsize[0] / 4.0
+        fcenter += d * fsize / 4.0
+        tcenter += d * fsize / 4.0
+        tcenter[i] -= 2 * d * fsize[i] / 4.0
+        ax.arrow(*np.hstack([fcenter, (tcenter - fcenter)]), color='b', width=.01,
+                 head_width=.1, head_starts_at_zero=False)
+    else:
+        ax.plot(*fcenter, color='b', marker='x', markersize=10)
 
-
-
+def _box_dims(partition, state):
+    bounds = np.array([[partition[i][s], partition[i][s+1]]
+                       for i, s in enumerate(state)])
+    return np.mean(bounds, axis=1), bounds[:,1] - bounds[:,0]
 
 def _draw_grid(partition, ax):
-    limits = [[p[0], p[-1]] for p in partition]
+    limits = np.array([[p[0], p[-1]] for p in partition])
     for i, p in enumerate(partition):
-        data = list(limits)
+        data = limits.copy()
         for x in p:
             data[i][0] = data[i][1] = x
-            ax.plot(data[0], data[1], color='k', linestyle='- ', linewidth=2)
+            ax.plot(data[0], data[1], color='k', linestyle='--', linewidth=2)
 
 
 
