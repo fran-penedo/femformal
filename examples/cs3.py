b3a1022f49d6c4a4208ea41722874dbad9df503f
Francisco Penedo <fran.penedo@gmail.com>
2016-11-08 11:48:22 -0500

    Modified cs3

--- examples/cs3.py
+++ examples/cs3.py
@@ -8,13 +8,13 @@ T = [10.0, 100.0]
 system, xpart, partition = fem.heatlinfem(N, L, T)
 
 v = fem.diag(N, 9, -1)
-apc1 = u.APCont([0, 500], 1, lambda x: 85)
+apc1 = u.APCont([0, 1000], 1, lambda x: 85 if x <= 500 else 125)
 apc2 = u.APCont([500, 1000], 1, lambda x: 125)
 apc3 = u.APCont([0, 1000], 1, lambda x: 25)
 regions = {'A': u.ap_cont_to_disc(apc1, xpart),
            'B': u.ap_cont_to_disc(apc2, xpart),
            'C': u.ap_cont_to_disc(apc3, xpart)}
-spec = "G (A) & G (B)"
+spec = "G (A)"
 init_states = [v]
 
 depth = 1
