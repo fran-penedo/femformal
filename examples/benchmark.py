01e06c769bf1e05bfd9b4e855ca70f24ca0237a8
Francisco Penedo <fran.penedo@gmail.com>
2017-06-23 16:43:20 -0400

    Refactored benchmark

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -1,5 +1,5 @@
 import argparse
-import importlib
+import imp
 import logging
 from timeit import default_timer as timer
 
@@ -10,7 +10,7 @@ from femformal.femts.verify import verify, verify_input_constrained
 
 logger = logging.getLogger('FEMFORMAL')
 
-def run_cs_draw(m, args):
+def run_abstract(m, args):
     logger.debug(m.system)
     start = timer()
     res = verify_input_constrained(m.system, m.partition, m.regions, m.init_states, m.spec, m.depth,
@@ -22,7 +22,7 @@ def run_cs_draw(m, args):
     print 'Res: {}'.format(res)
     print 'Time {}'.format(finish - start)
 
-def run_cs_milp(m, args):
+def run_milp(m, args):
     # logger.debug(m.system)
     start = timer()
     res = verify_singleton(m.dsystem, m.d0, m.spec, m.fdt_mult)
@@ -30,7 +30,7 @@ def run_cs_milp(m, args):
     print 'Res: {}'.format(res)
     print 'Time {}'.format(finish - start)
 
-def run_cs_milp_set(m, args):
+def run_milp_set(m, args):
     cs = m.cs
     start = timer()
     res = verify_set(cs.dsystem, cs.pset, cs.f, cs.spec, cs.fdt_mult)
@@ -38,7 +38,7 @@ def run_cs_milp_set(m, args):
     print 'Res: {}'.format(res)
     print 'Time {}'.format(finish - start)
 
-def run_cs_milp_synth(m, args):
+def run_milp_synth(m, args):
     cs = m.cs
     start = timer()
     res, synths = synthesize(cs.dsystem, cs.pset, cs.f, cs.spec, cs.fdt_mult)
@@ -50,7 +50,7 @@ def run_cs_milp_synth(m, args):
 def run_load(m, args):
     pass
 
-def run_cs_milp_batch(m, args):
+def run_milp_batch(m, args):
     res = []
     times = []
     trues = []
@@ -70,7 +70,7 @@ def run_cs_milp_batch(m, args):
     print "results: {}".format(res)
     print "true results: {}".format(trues)
 
-def run_cs_time(m, args):
+def run_abstract_batch(m, args):
     times = []
     its = 20
     for i in range(its):
@@ -85,19 +85,27 @@ def run_cs_time(m, args):
     print "verify times: max {0} min {1} avg {2}".format(
         max(times), min(times), sum(times) / float(its))
 
+def run_draw(m, args):
+    pass
+
 
 def get_argparser():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     subparsers = parser.add_subparsers(dest='action')
-    parser_draw = subparsers.add_parser(
-        'draw', help='Run an example once and plot evolution')
-    parser_draw.add_argument('-f', '--draw-file-prefix',
+    parser_abstract = subparsers.add_parser(
+        'abstract', help='Run an example once and plot evolution')
+    parser_abstract.add_argument('-f', '--draw-file-prefix',
                              help='plots are saved to svg files with this prefix')
-    parser_draw.add_argument('--draw-constr-ts', action='store_true')
-    parser_time = subparsers.add_parser(
-        'time', help='Run an example a number of times a show execution times')
+    parser_abstract.add_argument('--draw-constr-ts', action='store_true')
+    parser_abstract_batch = subparsers.add_parser(
+        'abstract_batch', help='Run an example a number of times a show execution times')
+    parser_draw = subparsers.add_parser(
+        'draw', help='Plot PDE trajectories')
+    parser_draw.add_argument('-f', '--draw-file-prefix',
+                             help='save plots to files with this prefix')
+    parser_draw.add_argument('-i', '--input-file', help='file with extra inputs')
     parser_milp = subparsers.add_parser(
         'milp', help='Run an example using MILP')
     parser_milp_set = subparsers.add_parser(
@@ -108,31 +116,27 @@ def get_argparser():
         'milp_synth', help='Run an example for synthesis using MILP')
     parser_load = subparsers.add_parser(
         'load', help='Load a benchmark file')
-    parser.add_argument('module', help='module containing the case study')
+    parser.add_argument('module', help='file containing the case study')
     parser.add_argument('-v', '--verbosity', action='count')
     parser.add_argument('--check-inv', action='store_true')
     return parser
 
+def load_module(f):
+    try:
+        return imp.load_source("femformal_benchmark_module", args.module)
+    except Exception as e:
+        print "Couldn't load module {}".format(args.module)
+        print e
+        return None
 
 def main():
     parser = get_argparser()
     args = parser.parse_args()
-    module = importlib.import_module(args.module)
-    if args.action == 'draw':
-        # command = "run_cs_draw(module, args)"
-        # cProfile.runctx(command, globals(), locals(), filename="prof.profile")
-        run_cs_draw(module, args)
-    elif args.action == 'time':
-        run_cs_time(module, args)
-    elif args.action == 'milp':
-        run_cs_milp(module, args)
-    elif args.action == 'milp_set':
-        run_cs_milp_set(module, args)
-    elif args.action == 'milp_synth':
-        run_cs_milp_synth(module, args)
-    elif args.action == 'milp_batch':
-        run_cs_milp_batch(module, args)
-    elif args.action == 'load':
-        run_load(module, args)
+    runstr = 'run_' + args.action
+    module = load_module(args.module)
+    if module is None:
+        exit()
+    if runstr in globals():
+        globals()[runstr](module, args)
     else:
         parser.print_help()
