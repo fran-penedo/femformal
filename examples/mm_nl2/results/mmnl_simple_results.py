a127e6aeee01d83d693045c7aaf8478c9a5f70c4
Francisco Penedo <fran.penedo@gmail.com>
2019-05-29 10:39:19 -0400

    Set draw options for mm_nl2 example

--- examples/mm_nl2/results/mmnl_simple_results.py
+++ examples/mm_nl2/results/mmnl_simple_results.py
@@ -1,8 +1,8 @@
-from examples.mm_nl.results.draw_opts import draw_opts
+from examples.mm_nl2.results.draw_opts import draw_opts
 
 
-# draw_opts['file_prefix'] = 'temp_plots/mm_yield2'
-# ts = [0.0, 0.2, 0.48]
+draw_opts['file_prefix'] = 'temp_plots/mm_yield3'
+ts = [0.0, 0.19]
 
 robustness = 0.0338482767663
 inputs = [0.0, 31.82766536141028, -50.56704562198145, 19.637252038483187, 100.0, -100.0, -100.0, -100.0, -100.0, -100.0, -100.0]
