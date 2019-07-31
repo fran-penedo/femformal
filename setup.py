ea169af7610bb1b6783f433e4e2c47ff04773ab6
Francisco Penedo <fran.penedo@gmail.com>
2019-07-31 16:52:59 -0400

    Improving requirement management and setup

--- setup.py
+++ setup.py
@@ -4,15 +4,21 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Formal Methods for PDEs',
-    'url': '',
-    'author': 'Fran Penedo',
-    'author_email': 'franp@bu.edu',
-    'version': '0.1.1',
-    'install_requires': [],
-    'packages': ['femformal'],
-    'scripts': [],
-    'name': 'femformal'
+    "description": "Formal Methods for PDEs",
+    "url": "",
+    "author": "Fran Penedo",
+    "author_email": "franp@bu.edu",
+    "version": "0.1.1",
+    "install_requires": [
+        "stlmilp[milp] >=1.0.1, <2",
+        "enum34>=1.1.6",
+        "matplotlib>=1.5.2",
+        "numpy>=1.11.1",
+        "scipy>=0.17.1",
+    ],
+    "packages": ["femformal"],
+    "scripts": [],
+    "name": "femformal",
 }
 
 setup(**config)
