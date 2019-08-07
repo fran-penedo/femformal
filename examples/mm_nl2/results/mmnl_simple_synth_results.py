afe0df3c02567334318edf21ae6b2a570fc74df0
Francisco Penedo <fran.penedo@gmail.com>
2019-08-07 11:49:03 -0400

    Ran mmnl2 example

--- examples/mm_nl2/results/mmnl_simple_synth_results.py
+++ examples/mm_nl2/results/mmnl_simple_synth_results.py
@@ -1,3 +1,8 @@
+from examples.mm_nl2.results.draw_opts import draw_opts
+
+
+draw_opts["file_prefix"] = "temp_plots/mm_yield3"
+ts = [0.0, 0.19]
 # Fri 02 Aug 2019 03:10:48 PM EDT v0.1.2-4-gd43ff49 Intel(R) Core(TM) i7-5930K CPU @ 3.50GHz 16641MB RAM
 # python run_benchmark.py --log-level INFO --gthreads 10 --goutputflag 0 milp_synth examples/mm_nl2/mmnl_simple_synth.py
 # Fri 02 Aug 2019 03:14:41 PM EDT v0.1.2-4-gd43ff49-dirty Intel(R) Core(TM) i7-5930K CPU @ 3.50GHz 16641MB RAM
@@ -17,5 +22,22 @@ inputs = [
     -100.00000000000006,
 ]
 time = 1404.17401314
-#Tue 06 Aug 2019 03:04:09 PM EDT v0.1.2-4-gd43ff49-dirty Intel(R) Core(TM) i7-5930K CPU @ 3.50GHz 16641MB RAM
-#python run_benchmark.py --log-level INFO --gthreads 10 --goutputflag 0 milp_synth examples/mm_nl2/mmnl_simple_synth.py
+# Tue 06 Aug 2019 03:04:09 PM EDT v0.1.2-4-gd43ff49-dirty Intel(R) Core(TM) i7-5930K CPU @ 3.50GHz 16641MB RAM
+# python run_benchmark.py --log-level INFO --gthreads 10 --goutputflag 0 milp_synth examples/mm_nl2/mmnl_simple_synth.py
+# Tue 06 Aug 2019 03:06:37 PM EDT v0.1.2-5-gb357a5b Intel(R) Core(TM) i7-5930K CPU @ 3.50GHz 16641MB RAM
+# python run_benchmark.py --log-level INFO --gthreads 10 --goutputflag 0 milp_synth examples/mm_nl2/mmnl_simple_synth.py
+robustness = 0.044706459582
+inputs = [
+    0.0,
+    43.73214238532524,
+    -100.0,
+    98.2820456106841,
+    -100.0,
+    -100.0,
+    -100.0,
+    -100.0,
+    -100.0,
+    -100.0,
+    -100.0,
+]
+time = 1192.84919786
